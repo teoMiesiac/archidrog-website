b17e241b01b7db405e1930ad5208f885e814e97d
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-09-15 15:12:44 +0200

    footer init

---
+++ src/sections/Footer/Footer.component.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { FooterWrapper } from './Footer.styles'
+
+const Footer = (): JSX.Element => <FooterWrapper>lol</FooterWrapper>
+
+export default Footer
---
+++ src/sections/Footer/Footer.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from './Footer.component'
+
+const renderComponent = () => {
+  return <Footer />
+}
+
+export default {
+  title: 'Sections|Footer'
+}
+
+export const Default = () => renderComponent()
