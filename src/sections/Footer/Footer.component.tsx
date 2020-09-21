9612c336c8d761f1a0fd544d7fd894bd2eef478c
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-09-21 23:46:15 +0200

    added contact component & RWD

--- src/sections/Footer/Footer.component.tsx
+++ src/sections/Footer/Footer.component.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { FooterWrapper } from './Footer.styles'
-import { Flex } from 'reflexbox/styled-components'
+import { Flex, Box } from 'reflexbox/styled-components'
 import { ArchidrogLogo } from '~/components/ArchidrogLogo'
+import { ContactFooter } from '~/components/ContactFooter'
 
 const Footer = (): JSX.Element => (
   <FooterWrapper>
-    <Flex>
-      <ArchidrogLogo wMobile={131} />
+    <Flex
+      flexDirection={['column', 'row']}
+      justifyContent={['', 'space-between']}
+      width="100%"
+      px={[15, 30]}
+      py={[20, 30, 50]}
+      maxWidth={1480}
+    >
+      <Box pb={['20px']}>
+        <ArchidrogLogo wMobile={131} />
+      </Box>
+      <ContactFooter />
     </Flex>
   </FooterWrapper>
 )
--- src/sections/Footer/Footer.styles.ts
+++ src/sections/Footer/Footer.styles.ts
@@ -3,4 +3,7 @@ import styled from 'styled-components'
 export const FooterWrapper = styled.footer`
   width: 100%;
   background-color: gainsboro;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
