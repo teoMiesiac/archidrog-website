36f552d5766ecb52eff248b96f49810fbeec8780
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-09-21 22:52:03 +0200

    RWD styling

--- src/components/ContactFooter/ContactFooter.component.tsx
+++ src/components/ContactFooter/ContactFooter.component.tsx
@@ -1,25 +1,56 @@
 import React from 'react'
 import { Flex, Box } from 'reflexbox/styled-components'
+import { IconWrapper, CategoryTitle } from './ContactFooter.styles'
+import { Address } from '~/components/Icons/Address'
+import { Email } from '~/components/Icons/Email'
+import { Fax } from '~/components/Icons/Fax'
+import { Phone } from '~/components/Icons/Phone'
 import { contactDetails } from '../../../fixtures/contactDetails'
 
 const ContactFooter = (): JSX.Element => (
-  <Flex flexDirection={['column', 'column', 'row']}>
-    <Flex flexDirection={['column']}>
-      <Box>{contactDetails.address}</Box>
-      <Box>
+  <Flex flexDirection={['column', 'row', 'row']}>
+    <Flex flexDirection={['column']} mr={['0', '30px', '50px']}>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <IconWrapper>
+          <Address />
+        </IconWrapper>
+        {contactDetails.address}
+      </Box>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <IconWrapper />
         {contactDetails.zipCode} {contactDetails.city}
       </Box>
     </Flex>
-    <Flex flexDirection={['column']}>
-      <Box>{contactDetails.email}</Box>
-      <Box>{contactDetails.phone}</Box>
-      <Box>{contactDetails.fax}</Box>
+    <Flex flexDirection={['column']} mr={['0', '30px', '50px']}>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <IconWrapper>
+          <Email />
+        </IconWrapper>
+        {contactDetails.email}
+      </Box>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <IconWrapper>
+          <Phone />
+        </IconWrapper>
+        {contactDetails.phone}
+      </Box>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <IconWrapper>
+          <Fax />
+        </IconWrapper>
+        {contactDetails.fax}
+      </Box>
     </Flex>
     <Flex flexDirection={['column']}>
-      <Box>{contactDetails.nip}</Box>
-      <Box>{contactDetails.regon}</Box>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <CategoryTitle>NIP</CategoryTitle>&nbsp;
+        {contactDetails.nip}
+      </Box>
+      <Box display="inline-box" verticalAlign="center" lineHeight="15px" fontSize="1.5rem" py={['5px', '8px']}>
+        <CategoryTitle>REGON</CategoryTitle>&nbsp;
+        {contactDetails.regon}
+      </Box>
     </Flex>
-    <Flex flexDirection={['column']}>lol2</Flex>
   </Flex>
 )
 
---
+++ src/components/ContactFooter/ContactFooter.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { Color } from '~/styles/constants'
+import { themeColor } from '~/styles/getters'
+
+export const IconWrapper = styled.div`
+  height: 15px;
+  width: 17px;
+  margin-right: 10px;
+`
+export const CategoryTitle = styled.span`
+  color: ${themeColor(Color.SECONDARY)};
+  font-weight: bold;
+  letter-spacing: 0.4px;
+`
