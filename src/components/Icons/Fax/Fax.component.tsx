37cbbd610500e1e8a95f63813e8820aad0dd617a
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-09-21 18:22:32 +0200

    footer icons

---
+++ src/components/Icons/Fax/Fax.component.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Fax = (): JSX.Element => (
+  <svg xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" viewBox="0 0 17 15" className="icon">
+    <path
+      fill="#AFC90C"
+      fillRule="evenodd"
+      className="path"
+      d="M13.461 9.997V15h-9.92V9.997h9.92zm-.711 2.86h-8.5v1.429h8.5v-1.429zM2.835 3.574v2.855l11.339.01V3.582h.854c1.089 0 1.972.92 1.972 2.053v5.793c0 .4-.312.725-.698.725h-2.128V9.295l-11.339-.01v2.859l-2.138.001c-.385 0-.697-.325-.697-.725V5.627c0-1.133.882-2.052 1.972-2.052l.863-.001zm9.915 7.14H4.252v1.429h8.498v-1.429zM13.461 0v5.714h-9.92V0h9.92z"
+    />
+  </svg>
+)
+
+export default Fax
---
+++ src/components/Icons/Fax/index.ts
@@ -0,0 +1,1 @@
+export { default as Fax } from './Fax.component'
