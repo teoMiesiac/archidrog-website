cafc7a49e7d4f5ea86a56e8b42bc1697cd510e6e
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-10-13 20:13:41 +0200

    Fotter background update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { Layout } from '~/layouts/Layout'
+import { WhoWeAreSection } from '~/sections/WhoWeAreSection'
 
-const IndexPage = () => <Layout>lol</Layout>
+const IndexPage = () => (
+  <Layout>
+    <WhoWeAreSection />
+  </Layout>
+)
 
 export default IndexPage
--- src/sections/Footer/Footer.styles.ts
+++ src/sections/Footer/Footer.styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
 
-export const FooterWrapper = styled.footer`
+export const FooterWrapper = styled(BackgroundImage)`
   width: 100%;
-  background-color: gainsboro;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-position: center center;
+  background-repeat: repeat-y;
+  background-size: cover;
 `
