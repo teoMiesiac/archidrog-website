4ff09fbd8d35e6c06bd318fcd3a6be510cb4c1bf
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-10-15 18:10:36 +0200

    page update

--- src/pages/offer.tsx
+++ src/pages/offer.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { Layout } from '~/layouts/Layout'
+import { Offer } from '~/sections/Offert'
 
-const IndexPage = () => <Layout>lol</Layout>
+const IndexPage = () => (
+  <Layout>
+    <Offer />
+  </Layout>
+)
 
 export default IndexPage
