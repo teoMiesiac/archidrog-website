37b8bda689b178650240126c124dbce4f730de38
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-12-01 16:32:23 +0100

    added section

--- src/pages/realizations.tsx
+++ src/pages/realizations.tsx
@@ -1,45 +1,11 @@
-import * as React from 'react'
+import React from 'react'
 import { Layout } from '~/layouts/Layout'
-import { useStaticQuery, graphql } from 'gatsby'
+import { Realizations } from '~/sections/Realizations'
 
-export const query = graphql`
-  query {
-    subRealizations: allMarkdownRemark(filter: { frontmatter: { type: { eq: "sub-realization" } } }) {
-      edges {
-        node {
-          frontmatter {
-            title
-            sub_realizations
-            images {
-              id
-              childImageSharp {
-                fluid(maxWidth: 800, maxHeight: 600) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            bullet_points
-          }
-        }
-      }
-    }
-    realizations: allMarkdownRemark(filter: { frontmatter: { type: { eq: "realization" } } }) {
-      edges {
-        node {
-          frontmatter {
-            title
-            sub_realizations
-          }
-        }
-      }
-    }
-  }
-`
-
-const IndexPage = () => {
-  const data = useStaticQuery(query)
-  console.log(data)
-  return <Layout>lol</Layout>
-}
+const IndexPage = () => (
+  <Layout>
+    <Realizations />
+  </Layout>
+)
 
 export default IndexPage
