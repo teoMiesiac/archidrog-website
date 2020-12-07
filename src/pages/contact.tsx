5a265ff50e5c98546899d0c60e0c521be1149234
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2020-12-07 20:36:19 +0100

    contact form page update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
 import { Layout } from '~/layouts/Layout'
+import { Contact } from '~/sections/Contact'
+import { ContactForm } from '~/sections/ContactForm'
 
-const IndexPage = () => <Layout>lol</Layout>
+const IndexPage = () => (
+  <Layout>
+    <Contact />
+    <ContactForm />
+  </Layout>
+)
 
 export default IndexPage
