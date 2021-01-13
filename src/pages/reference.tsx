a2f0f9d787a66b250fb15f90ec00aebb8f79ba66
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2021-01-13 21:49:32 +0100

    added section

--- src/pages/reference.tsx
+++ src/pages/reference.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { Layout } from '~/layouts/Layout'
+import { References } from '~/sections/References'
 
-const IndexPage = () => <Layout>lol</Layout>
+const IndexPage = () => (
+  <Layout>
+    <References />
+  </Layout>
+)
 
 export default IndexPage
