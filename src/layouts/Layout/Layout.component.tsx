2115a57c2d48768f141a7b47f85d076c24d0eeda
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2021-03-04 15:16:14 +0100

    suport for react reveal ssr

--- src/layouts/Layout/Layout.component.tsx
+++ src/layouts/Layout/Layout.component.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { LayoutRoot } from '~/layouts/LayoutRoot'
 import { LayoutMain } from '~/layouts/LayoutMain'
 import '~/styles/global.css'
+import config from 'react-reveal/globals'
+
+config({ ssrFadeout: true })
 
 const Layout = ({ children }): JSX.Element => (
   <LayoutRoot>
