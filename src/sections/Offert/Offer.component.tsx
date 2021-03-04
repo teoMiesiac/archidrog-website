1e62c20db695718b819df68489333be82db8b9ef
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2021-03-04 15:18:19 +0100

    paralax added

--- src/sections/Offert/Offer.component.tsx
+++ src/sections/Offert/Offer.component.tsx
@@ -1,31 +1,68 @@
 import React from 'react'
 import { Flex } from 'reflexbox/styled-components'
+import Fade from 'react-reveal/Fade'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
+import { ParallaxBanner } from 'react-scroll-parallax'
 import { Section } from '~/components/Section'
 import { Offer as SingleOffer } from '~/components/Offer'
 import { SectionTitle } from '~/components/SectionTitle'
-import { SubTitle, Bold } from './Offer.styles'
+import { SubTitle, Bold, BackgroundMask } from './Offer.styles'
 import { offers } from '../../../fixtures/offers'
 
-const Offer = (): JSX.Element => (
-  <Section>
-    <Flex
-      flexDirection={['column', 'column']}
-      flexWrap="nowrap"
-      px={[15, 30]}
-      py={[20, 30, 50]}
-      maxWidth={1480}
-      width="100%"
-      justifyContent={['', 'space-between']}
+export const query = graphql`
+  query {
+    file1: file(relativePath: { eq: "images/foto1.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 3543, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+const Offer = (): JSX.Element => {
+  const data = useStaticQuery(query)
+
+  return (
+    <ParallaxBanner
+      layers={[
+        {
+          children: <Img fluid={data.file1.childImageSharp.fluid} style={{ height: '100%' }} />,
+          amount: 0.3
+        },
+        { children: <BackgroundMask />, amount: 0 }
+      ]}
+      style={{
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
     >
-      <SectionTitle>Oferta</SectionTitle>
-      <SubTitle margin={['20px 0']}>
-        <Bold>Pracownia Projektowa ARCHIDROG</Bold> świadczy usługi w branży drogowej i mostowej w zakresie:
-      </SubTitle>
-      {offers.map(offer => (
-        <SingleOffer key={offer.title} title={offer.title} link={offer.link} subOffers={offer.subOffers} />
-      ))}
-    </Flex>
-  </Section>
-)
+      <Section>
+        <Flex
+          flexDirection={['column', 'column']}
+          flexWrap="nowrap"
+          px={[15, 30]}
+          py={[20, 30, 150]}
+          maxWidth={1480}
+          width="100%"
+          justifyContent={['', 'space-between']}
+        >
+          <SectionTitle>Oferta</SectionTitle>
+          <SubTitle margin={['20px 0']}>
+            <Bold>Pracownia Projektowa ARCHIDROG</Bold> świadczy usługi w branży drogowej i mostowej w zakresie:
+          </SubTitle>
+          <Fade left>
+            {offers.map(offer => (
+              <SingleOffer key={offer.title} title={offer.title} link={offer.link} subOffers={offer.subOffers} />
+            ))}
+          </Fade>
+        </Flex>
+      </Section>
+    </ParallaxBanner>
+  )
+}
 
 export default Offer
--- src/sections/Offert/Offer.styles.tsx
+++ src/sections/Offert/Offer.styles.tsx
@@ -15,3 +15,16 @@ export const SubTitle = styled.p<MarginProp>`
 export const Bold = styled.span`
   font-weight: bold;
 `
+
+export const BackgroundMask = styled.div`
+  width: 100%;
+  object-fit: contain;
+  height: 100%;
+  background-image: linear-gradient(to bottom, #ffffff, rgba(246, 246, 246, 0.95) 50%, #ffffff);
+  background-position: 0 0;
+  background-repeat: repeat-y;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
